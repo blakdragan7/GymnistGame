4549eee22563b6bdb05f3d5436d90692239e8f7d
Jonathon Copeland <jcopela4@gmail.com>
2017-06-12 23:05:53 -0400

    started implementing Swing Physics

--- Source/GymnastGame/SwingMovementComponenet.cpp
+++ Source/GymnastGame/SwingMovementComponenet.cpp
@@ -2,15 +2,80 @@
 
 #include "SwingMovementComponenet.h"
 
+USwingMovementComponenet::USwingMovementComponenet()
+{
+	bHasInput = false;
+	swingDrag = 0;
+}
+
 void USwingMovementComponenet::PhysCustom(float deltaTime, int32 Iterations)
 {
-    
+	SwingPhys(deltaTime, Iterations);
 }
 
 void USwingMovementComponenet::SwingPhys(float deltaTime,int32 Iterations)
 {
-    FVector centerLocation = swingCenter->GetActorLocation();
-    FVector myLocation = GetOwner()->GetActorLocation();
-    
-    float length = (myLocation - centerLocation).Size();
+	
+	if (IsValid(swingCenter))
+	{
+		FVector myLocation = GetOwner()->GetActorLocation();
+		FVector ActorCurrentUp = GetOwner()->GetActorUpVector();
+		// Face Actor Up Vector To Swing Center
+		FVector SwingCenterLocation = swingCenter->GetActorLocation();
+		FVector radius = SwingCenterLocation - myLocation;
+
+		FRotator newRotation = FRotationMatrix::MakeFromXZ(radius, ActorCurrentUp).Rotator();
+		GetOwner()->SetActorRotation(newRotation);
+
+		FVector ActorCurrentTangentVector = (RestingLocation - myLocation).GetSafeNormal();
+
+		// Calculate Swing Velocity and Update Actor Position
+		float dotProduct = FVector::DotProduct(StartingRadius.GetSafeNormal(), radius.GetSafeNormal());
+		float angle = acos(dotProduct);
+		angle = FMath::IsNearlyZero(angle,.001f) ? 0 : angle;
+		float Tension = (Velocity.SizeSquared() / RadiusLength) + (980 * cos(angle));
+		Velocity += radius.SafeNormal() * Tension * deltaTime;
+		Velocity += (myLocation - SwingCenterLocation).GetSafeNormal() * 980 * cos(angle)*deltaTime;
+		Velocity += ActorCurrentTangentVector * 980 * sin(angle)*deltaTime;
+
+		GEngine->AddOnScreenDebugMessage(0, 0.5f, FColor::Yellow, ActorCurrentUp.ToString());
+		GEngine->AddOnScreenDebugMessage(1, 0.5f, FColor::Yellow, ActorCurrentTangentVector.ToString());
+		GEngine->AddOnScreenDebugMessage(2, 0.5f, FColor::Yellow, FString::Printf(TEXT("%f"), dotProduct));
+		GEngine->AddOnScreenDebugMessage(3, 0.5f, FColor::Yellow, FString::Printf(TEXT("%f"), angle));
+
+		Velocity *= 0.99;
+		if (bHasInput)
+		{
+			bHasInput = false;
+			Velocity += currentSwingVelocity * deltaTime;
+			currentSwingVelocity = FVector();
+		}
+
+		FVector newLocation = myLocation + Velocity;
+		GetOwner()->SetActorLocation(newLocation);
+	}
+}
+
+void USwingMovementComponenet::AddSwingImpusle(const FVector velocity)
+{
+	currentSwingVelocity += velocity;
+	bHasInput = true;
+}
+
+void USwingMovementComponenet::SetSwingCenterActor(const AActor * SwingActor)
+{
+	check(SwingActor);
+	swingCenter = SwingActor;
+
+	FVector centerLocation = swingCenter->GetActorLocation();
+	FVector myLocation = GetOwner()->GetActorLocation();
+
+	StartingRadius = (centerLocation - myLocation);
+	RadiusLength = StartingRadius.Size();
+	// Face Actor Up Vector To Swing Center
+	FRotator newRotation = FRotationMatrix::MakeFromX(StartingRadius).Rotator();
+	GetOwner()->SetActorRotation(newRotation);
+	
+	StartingActorUpVector = GetOwner()->GetActorUpVector();
+	RestingLocation = myLocation;
 }
--- Source/GymnastGame/SwingMovementComponenet.h
+++ Source/GymnastGame/SwingMovementComponenet.h
@@ -7,18 +7,34 @@
 #include "SwingMovementComponenet.generated.h"
 
 /**
- * 
+ * MovementComponent Extension to add Swing physics for Actors
  */
 UCLASS()
 class GYMNASTGAME_API USwingMovementComponenet : public UCharacterMovementComponent
 {
 	GENERATED_BODY()
-	
+private:
+	FVector StartingActorUpVector;
+	FVector RestingLocation;
+	FVector StartingRadius;
+	double swingDrag;
+	double RadiusLength;
+	FVector currentSwingVelocity;
+	int32	bHasInput:1;
+
 protected:
 	virtual void PhysCustom(float deltaTime, int32 Iterations) override;
     virtual void SwingPhys(float deltaTime,int32 Iterations);
     
 public:
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Swing Physics")
-    AActor* swingCenter;
+	USwingMovementComponenet();
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Swing Physics")
+    const AActor* swingCenter;
+
+	UFUNCTION(BlueprintCallable,Category= "Swing")
+	void AddSwingImpusle(const FVector velocicy);
+
+	UFUNCTION(BlueprintCallable, Category = "Swing")
+	void SetSwingCenterActor(const AActor *SwingActor);
 };
