d6401dc3da1e183f5334d742b330546a1704cdb6
Jonathon Copeland <jcopela4@gmail.com>
2017-07-01 11:40:03 -0400

    added clamp for alpha value

--- Source/GymnastGame/GymnastGameCharacter.cpp
+++ Source/GymnastGame/GymnastGameCharacter.cpp
@@ -125,6 +125,7 @@ void AGymnastGameCharacter::Tick(float DeltaTime)
 			{
 				FRotator currentRotation = StartingBoomRotation;
 				float alpha = (CurrentActorLocation.Z / CameraRotationPositionRange);
+				alpha = FMath::Clamp<float>(alpha, 0, 1);
 				currentRotation.Pitch += FMath::Sign(alpha) * FMath::Lerp<float,float>(FMath::Abs(alpha),0, CameraRotationRotationRange) ;
 				CameraBoom->SetRelativeRotation(currentRotation);
 			}
